57bb4b4470efede0a1c2182c331d61711dc5933c
Maxence <mbillon.sio@gmail.com>
2025-12-01 16:23:39 +0100

    Getter/Setter Hebergement

--- back/classes/Hebergement.php
+++ back/classes/Hebergement.php
@@ -22,6 +22,76 @@ class Hebergement
         $this->ville = $ville;
         $this->capacite = $capacite;
     }
+
+    public function GetId()
+    {
+        return $this->id;
+    }
+
+    public function GetNom()
+    {
+        return $this->nom;
+    }
+
+    public function GetEtoiles()
+    {
+        return $this->etoiles;
+    }
+
+    public function GetRue()
+    {
+        return $this->rue;
+    }
+
+    public function GetCp()
+    {
+        return $this->cp;
+    }
+
+    public function GetVille()
+    {
+        return $this->ville;
+    }
+
+    public function GetCapacite()
+    {
+        return $this->capacite;
+    }
+
+    public function SetId()
+    {
+        $this->id = $id;
+    }
+
+    public function SetNom()
+    {
+        $this->nom = $nom;
+    }
+
+    public function SetEtoiles()
+    {
+        $this->etoiles = $etoiles;
+    }
+
+    public function SetRue()
+    {
+        $this->rue = $rue;
+    }
+
+    public function SetCp()
+    {
+        $this->cp = $cp;
+    }
+
+    public function SetVille()
+    {
+        $this->ville = $ville;
+    }
+
+    public function SetCapacite()
+    {
+        $this->capacite = $capacite;
+    }
 }
 
 ?>
