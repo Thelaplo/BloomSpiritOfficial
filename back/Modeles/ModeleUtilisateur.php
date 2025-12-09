de26409ed25f9ce40d0e3a3cdaddf4b011edd846
Jenny <jdigonnaux.sio@gmail.com>
2025-12-09 11:54:36 +0100

    ajout modelAvis + utilisateur modif class avis

---
+++ back/Modeles/ModeleUtilisateur.php
@@ -0,0 +1,54 @@
+<?php
+
+class ModeleUtilisateur {
+
+
+    public static function SelectAll() : array 
+    {
+        include_once('./connexpdo.inc.php');
+        $cnx=connexpdo('bdExcursion','myparam');
+        $reqChaine="SELECT * FROM Utilisateur";
+        $requete=$cnx->prepare($reqChaine);
+        $result=$requete->execute();
+        $lesUtilisateurs = array();
+        while($unLigne =$requete->fetch(PDO::FETCH_ASSOC))
+        {
+            $login = $uneLigne['login'];
+            $mdp = $uneLigne['mdp'];
+            $isAdmin = $uneLigne['IsAdmin'];
+            $unUser = new Utilisateur($login, $mdp, $isAdmin);
+            array_push($lesUtilisateurs,$unUser);
+        }
+        $requete->closeCursor();
+        $cnx=null;
+        return $lesUtilisateurs;
+    }
+
+    public static function DeleteById(int $id) : void
+    {
+        include_once('./connexpdo.inc.php');
+        $cnx=connexpdo('bdExcursion','myparam');
+        $reqChaine="DELETE FROM Utilisateur WHERE id = :id";
+        $requete=$cnx->prepare($reqChaine);
+        $requete->BindParam(":id",$id,PDO::PARAM_INT);
+        $result=$requete->execute();
+        $requete->closeCursor();
+        $cnx=null;
+    }
+
+    public static function Update(Utilisateur $utilisateur) : void
+    {
+        include_once('./connexpdo.inc.php');
+        $cnx=connexpdo('bdExcursion','myparam');
+        $reqChaine="UPDATE Utilisateur SET login = :login, mdp = :mdp, IsAdmin = :IsAdmin WHERE id = :id";
+        $requete=$cnx->prepare($reqChaine);
+        $requete->BindParam(":login", $login,PDO::PARAM_STR);
+        $requete->BindParam(":mdp", $mdp, PDO::PARAM_STR);
+        $requete->BidnParam(":IsAdmin", $IsAdmin, PDO::PARAM_BOOL);
+        $result=$requete->execute();
+        $requete->closeCursor();
+        $cnx=null;
+    }
+}
+
+?>
--- back/classes/Avis.php
+++ back/classes/Avis.php
@@ -4,13 +4,17 @@ class Avis
 {
     private int $numero;
     private string $login;
+    private int $idExcursion;
     private string $contenu;
+    private int $note;
 
-    function __construct(int $numero, string $login, string $contenu)
+    function __construct(int $numero, string $login, int $idExcursion, string $contenu, int $note)
     {
         $this->numero = $numero;
         $this->login = $login;
+        $this->idExcursion = $idExcursion;
         $this->contenu = $contenu;
+        $this->note = $note;
     }
 
 }
